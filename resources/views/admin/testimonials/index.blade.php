f2605e628612b7629c938f9c89a293fd115a1628
FeniksZharovik <158120171+FeniksZharovik@users.noreply.github.com>
2026-05-22 22:42:34 +0700

    Responsive testimonial index avatar letter

--- resources/views/admin/testimonials/index.blade.php
+++ resources/views/admin/testimonials/index.blade.php
@@ -42,7 +42,8 @@
                                 <div class="flex items-center gap-3">
 
                                     <div
-                                        class="w-10 h-10 rounded-full bg-rose-l
+                                    class="w-10 h-10 aspect-square shrink-0
+                                    rounded-full bg-rose-l
                                     flex items-center justify-center
                                     text-rose-d font-semibold">
 
@@ -72,12 +73,11 @@
                                         class="w-4 h-4">
 
                                         <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006
-                    5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527
-                    1.258 5.273c.271 1.136-.964 2.033-1.96 1.425L12
-                    18.354l-4.632 2.826c-.996.608-2.231-.29-1.96-1.425
-                    l1.258-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305
-                    l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
-
+                                            5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527
+                                            1.258 5.273c.271 1.136-.964 2.033-1.96 1.425L12
+                                            18.354l-4.632 2.826c-.996.608-2.231-.29-1.96-1.425
+                                            l1.258-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305
+                                            l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
                                     </svg>
 
                                     <span class="font-medium text-brown">
